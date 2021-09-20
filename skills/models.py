2a46041b863f4bc2df67b907129cf50dfdb6038b
pythingale <pythingale@gmail.com>
2021-09-21 01:14:25 +0430

    function added to show techers for every skill

--- skills/models.py
+++ skills/models.py
@@ -5,6 +5,9 @@ from django_extensions.db.models import TimeStampedModel
 class SkillType(TimeStampedModel):
     type = models.CharField(max_length=24)
 
+    class Meta:
+        ordering = ["type"]
+
     def __str__(self):
         return self.type
 
@@ -12,6 +15,13 @@ class SkillType(TimeStampedModel):
 class Skill(TimeStampedModel):
     skill_type = models.ForeignKey(SkillType, on_delete=models.CASCADE)
     skill = models.CharField(max_length=24)
+
+    def get_teachers(self):
+        teachers = self.userwall_set.all()
+        return teachers
+
+    class Meta:
+        ordering = ["skill"]
 
     def __str__(self):
         return self.skill
